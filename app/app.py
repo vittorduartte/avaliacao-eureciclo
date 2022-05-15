b1d6800649e84ae6cce0f3846e7ee2f9d128403b
vittorduartte <mateusriograndense@gmail.com>
2022-05-15 14:36:05 -0300

    Registrando os recursos de envio na aplicação

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 from urllib import response
 from fastapi import FastAPI
 from .resources import index as index_routes
+from .resources import upload as upload_routes
 from .utils import database
+
 def create_app():
     database.create_tables()
     
     app = FastAPI()
     
     index_routes.init(app)
-    
+    upload_routes.init(app)
+
     return app
