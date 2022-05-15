d96a80d2ed8de962ca5256b77fcc0ef62c5d976d
vittorduartte <mateusriograndense@gmail.com>
2022-05-15 18:51:57 -0300

    Remoção das rotas upload e registro de sale

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from urllib import response
 from fastapi import FastAPI
 from .resources import index as index_routes
-from .resources import upload as upload_routes
+from .resources import sale as sale_routes
 from .utils import database
 
 def create_app():
@@ -10,6 +10,6 @@ def create_app():
     app = FastAPI()
     
     index_routes.init(app)
-    upload_routes.init(app)
+    sale_routes.init(app)
 
     return app
