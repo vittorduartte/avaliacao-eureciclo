abe6fd50e3fb5ab81efcc98fa2555ef6de8bf4b1
vittorduartte <mateusriograndense@gmail.com>
2022-05-15 18:42:19 -0300

    Atualizando o modelo para a abordagem mais simplificada
    Removendo a relação com a entidade Upload e corrigindo o tipo de dados do atributo create_at

--- app/models/sale.py
+++ app/models/sale.py
@@ -3,8 +3,6 @@ from sqlalchemy import Integer
 from sqlalchemy import Float
 from sqlalchemy import String
 from sqlalchemy import DateTime
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..extensions.database import Base
 
@@ -19,10 +17,8 @@ class Sale(Base):
     endereco = Column(String(100), nullable=False)
     fornecedor = Column(String(50), nullable=False)
     preco_unitario = Column(Float, nullable=False)
-    create_at = Column(DateTime, nullable=False, default=datetime.now().strftime('%Y-%m-%d'))
-    upload_id = Column(String(50), ForeignKey('upload.id'), nullable=False)
-    
-    upload = relationship("Upload", back_populates="sales")
+    total = Column(Float, nullable=False)
+    create_at = Column(DateTime, nullable=False, default=datetime.now())
 
     def __repr__(self):
-        return f"Sale(id={self.id}, comprador={self.comprador}, descricao={self.descricao}, quantidade={self.quantidade}, endereco={self.quantidade}, fornecedor={self.fornecedor}, preco_unitario={self.preco_unitario}, create_at={self.create_at}, upload_id={self.upload_id})"
+        return f"Sale(id={self.id}, comprador={self.comprador}, descricao={self.descricao}, quantidade={self.quantidade}, endereco={self.quantidade}, fornecedor={self.fornecedor}, preco_unitario={self.preco_unitario}, create_at={self.create_at})"
