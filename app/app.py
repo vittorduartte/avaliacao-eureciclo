875b5fe16226a755ffc356fa484544f3807e174e
vittorduartte <mateusriograndense@gmail.com>
2022-05-15 12:58:16 -0300

    Adicionando a criação de tabelas na função criadora da aplicação

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from urllib import response
 from fastapi import FastAPI
 from .resources import index as index_routes
-
+from .utils import database
 def create_app():
+    database.create_tables()
+    
     app = FastAPI()
-
+    
     index_routes.init(app)
-
+    
     return app
