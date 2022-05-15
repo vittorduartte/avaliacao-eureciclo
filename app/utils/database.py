94c15f520d6e7c02f96f96647ecd2d38f3c62995
vittorduartte <mateusriograndense@gmail.com>
2022-05-15 18:48:01 -0300

    Removendo a criação da tabela Upload da database util

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,14 +1,11 @@
 from ..extensions.database import myDatabase
-from ..models.upload import Base as UploadBase
 from ..models.sale import Base as SaleBase
-
 
 def create_tables():
     Conn = myDatabase.get_engine()
 
-    if not Conn.dialect.has_table(Conn.connect(), 'upload') or not Conn.dialect.has_table(Conn.connect(), 'sale'):
+    if not Conn.dialect.has_table(Conn.connect(), 'sale'):
         try:
-            UploadBase.metadata.create_all(bind=Conn)
             SaleBase.metadata.create_all(bind=Conn)
             print("\nCreated tables successfully!\n")
         except Exception as e:
