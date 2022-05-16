10d72bf84e844145f0345772e9761ff154b9c526
vittorduartte <mateusriograndense@gmail.com>
2022-05-15 22:17:12 -0300

    Retornar um preview data ao processar um arquivo txt

--- app/resources/sale.py
+++ app/resources/sale.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter
 from fastapi import UploadFile
 from fastapi import HTTPException
 from fastapi import Depends
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from ..controllers import sale
 from ..utils import files
@@ -14,13 +17,15 @@ def init(app):
         tags=["Vendas"]
     )
 
-    @router.post('/vendas/', response_model=dict)
-    async def create_sales(db: Session = Depends(myDatabase.get_session), file_upload: UploadFile = None):
+    templates = Jinja2Templates(directory="app/templates")
+
+    @router.post('/vendas/', response_class=HTMLResponse)
+    async def create_sales(request: Request, db: Session = Depends(myDatabase.get_session), file_upload: UploadFile = None):
         try:
             content_file = await file_upload.read()
-            data = files.scrapper(content_file)
-            sale.register(db, data["class"])
-            return {"data": data["dict"]}
+            sale_data = files.scrapper(content_file)
+            sale.register(db, sale_data["class"])
+            return templates.TemplateResponse("preview.html", {"request": request, "sales": sale_data["dict"], "total": sale_data["total"]})
 
         except Exception as e:
             raise HTTPException(
