4a807f25d5b76a75dd641167242443f527ea48a1
vittorduartte <mateusriograndense@gmail.com>
2022-05-15 22:06:03 -0300

    Registrando o ponto de montagem para servir arquivos estáticos

--- app/app.py
+++ app/app.py
@@ -1,14 +1,17 @@
+from re import template
 from urllib import response
 from fastapi import FastAPI
 from .resources import index as index_routes
 from .resources import sale as sale_routes
 from .utils import database
+from .extensions import static as static_files
 
 def create_app():
     database.create_tables()
     
     app = FastAPI()
     
+    static_files.init(app)
     index_routes.init(app)
     sale_routes.init(app)
 
