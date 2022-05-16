40341610bb61f94c07062837d313583e6452b241
vittorduartte <mateusriograndense@gmail.com>
2022-05-15 22:03:40 -0300

    Renderizando o template de envio no endereço raiz

--- app/resources/index.py
+++ app/resources/index.py
@@ -1,14 +1,19 @@
+from fastapi import Request
 from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 def init(app):
     router = APIRouter(
         tags=["Index"]
     )
 
-    @router.get("/", response_model=dict)
-    def index():
-        return {"Message": "Hello EuReciclo"}
-    
+    templates = Jinja2Templates(directory="app/templates")
+
+    @router.get("/", response_class=HTMLResponse)
+    async def index(request: Request):
+        return templates.TemplateResponse("index.html", {"request": request})
+
     app.include_router(router)
 
     return app
